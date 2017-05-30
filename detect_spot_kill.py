165ef9535dc7ae9be8fc32517ea1f785cf153ecd
Christophe Gasmi <rekcah78@gmail.com>
2017-05-30 15:18:52 +0200

    add detect_spot_kill.py

---
+++ detect_spot_kill.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import argparse, logging, ec2metadata, boto3
+
+
+parser = argparse.ArgumentParser(description='Multi runner backup to S3.')
+parser.add_argument('--dynamodb', required=True, help='name of dynamodb table')
+parser.add_argument('--log', '-L', required=True, help='Path of log file (default: /var/log/backup-s3.log)', default="/var/log/backup-s3.log")
+parser.add_argument('--queue', '-Q', required=True, help='Url of the SQS queue')
+
+args = parser.parse_args()
+
+logging.basicConfig(filename=str(args.log),format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+def get_kill(args):
+    while(True):
+        if ec2metadata.get('termination-time') is None:
+            sleep(10)
+        else:
+            instanceid = ec2metadata.get('instance-id')
+            region = os.environ["AWS_REGION"]
+            dynamodb = boto3.client('dynamodb',region_name=region)
+            #get current job
+            response = dynamodb.get_item(TableName=args.dynamodb, Key={'Instance':{'S':instanceid}})
+            directory = response['Item']['Instance']
+            queue_url = args.queue
+            #add in queue
+            try:
+                msg = sqs.send_message(
+                    QueueUrl=queue_url,
+                    DelaySeconds=10,
+                    MessageAttributes={
+                        'Title': {
+                            'DataType': 'String',
+                            'StringValue': directory
+                        }
+                    },
+                    MessageBody=(directory)
+                )
+                logging.info('Adding message '+msg['MessageId']+' in queue is done')
+            except:
+                logging.error('Adding message '+directory+' in queue is failed')
+
+get_kill(args)
